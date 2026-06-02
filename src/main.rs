84076a420d6e29177de75df8ef43fb44fc1a3b1e
jCabala <kuba.cabala6@gmail.com>
2026-06-02 09:51:56 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {    
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                // Send 8 bytes of data to the client
+                // 4 bytes of "size" with any value and 4 bytes of data representing num 7
+                let data: [u8; 8] = [0, 0, 0, 8, 0, 0, 0, 7];
+                _stream.write(&data).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
